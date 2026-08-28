3e28b605684e99f3f8293b25468301b7c3aca711
Al_Niz <sanfour2011@gmail.com>
2026-08-28 13:27:25 +0200

    Testing to accessing udev via File.OpenHandle() and RandomAccess.Read()

--- LiteFury.Acquisition/LiteFury.Acquisition.ConsoleApp/Program.cs
+++ LiteFury.Acquisition/LiteFury.Acquisition.ConsoleApp/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace LiteFury.Acquisition.ConsoleApp;
+﻿using LiteFury.Acquisition.Core;
+namespace LiteFury.Acquisition.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        using (var handle = File.OpenHandle(PcieDevice.BRAM_RESOURCE_FILE_DMA, FileMode.Open, FileAccess.Read))
+        {
+            var buffer = new byte[4];
+            RandomAccess.Read(handle, buffer, PcieDevice.BRAM_BASE_DMA);
+           
+            Console.WriteLine($"{BitConverter.ToUInt32(buffer, 0):X8}");
+            
+        }
     }
 }
--- LiteFury.Acquisition/LiteFury.Acquisition.Core/PcieDevice.cs
+++ LiteFury.Acquisition/LiteFury.Acquisition.Core/PcieDevice.cs
@@ -1,6 +1,6 @@
 namespace LiteFury.Acquisition.Core;
 
-public class PcieDevice
+public static class PcieDevice
 {
 //     // Sysfs path to the PCIe BAR2 resource, memory-mapped for register/BRAM access.
 // #define PCI_RESOURCE2_PATH "/sys/bus/pci/devices/0000:01:00.0/resource2"
