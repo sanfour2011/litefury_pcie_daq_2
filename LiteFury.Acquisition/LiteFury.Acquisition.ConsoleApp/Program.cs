14e2d895c5ddba9d458c23cb30d934fdf2fbb649
Al_Niz <sanfour2011@gmail.com>
2026-08-29 12:15:36 +0200

    wip finished trying to read device nodes with native .net functions

--- LiteFury.Acquisition/LiteFury.Acquisition.ConsoleApp/Program.cs
+++ LiteFury.Acquisition/LiteFury.Acquisition.ConsoleApp/Program.cs
@@ -1,40 +1,51 @@
-﻿using LiteFury.Acquisition.Core;
-using System.IO.MemoryMappedFiles;
+﻿using System.IO.MemoryMappedFiles;
 using System.Runtime.InteropServices;
+using LiteFury.Acquisition.Core;
 using LiteFury.Acquisition.Core.Interop;
 
 namespace LiteFury.Acquisition.ConsoleApp;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        using (var handle = File.OpenHandle(PcieDevice.BRAM_RESOURCE_FILE_DMA, FileMode.Open, FileAccess.Read))
+        using (var handle = File.OpenHandle(PcieDevice.BRAM_RESOURCE_FILE_DMA))
         {
             var buffer = new byte[2048 * 4 / 2];
             RandomAccess.Read(handle, buffer, PcieDevice.BRAM_A_BASE_DMA);
             Console.WriteLine("--- Buffer A ---");
-            for (int i = 0; i < buffer.Length; i = i + 4)
+            for (var i = 0; i < buffer.Length; i = i + 4)
                 Console.WriteLine($"{BitConverter.ToUInt32(buffer, i):X8}");
 
             Console.WriteLine("--- Buffer B ---");
             RandomAccess.Read(handle, buffer, PcieDevice.BRAM_B_BASE_DMA);
-            for (int i = 0; i < buffer.Length; i = i + 4)
+            for (var i = 0; i < buffer.Length; i = i + 4)
                 Console.WriteLine($"{BitConverter.ToUInt32(buffer, i):X8}");
         }
 
-        int fd = Libc.open(PcieDevice.CSR_RESOURCE_FILE, Libc.O_RDONLY | Libc.O_SYNC);
-        if (fd<0) 
+        var fd = Libc.open(PcieDevice.CSR_RESOURCE_FILE, Libc.O_RDONLY | Libc.O_SYNC);
+        if (fd < 0)
             Console.WriteLine($"Error fd={fd}!");
-        IntPtr map = Libc.mmap(IntPtr.Zero, (UIntPtr)8, Libc.PROT_READ, Libc.MAP_SHARED, fd, 0);
+        var map = Libc.mmap(IntPtr.Zero, 8, Libc.PROT_READ, Libc.MAP_SHARED, fd, 0);
         Libc.close(fd);
-        if (map == Libc.MAP_FAILED) 
+        if (map == Libc.MAP_FAILED)
             Console.WriteLine("MAP FAILED");
 
-        uint status_reg = (uint)Marshal.ReadInt32(map, 4);
-        Libc.munmap(map, (UIntPtr)8);
+        var status_reg = (uint)Marshal.ReadInt32(map, 4);
+        Libc.munmap(map, 8);
         Console.WriteLine($"status_reg: {status_reg}");
+
+        using var fs = new FileStream(
+            PcieDevice.USR_IRQ_EVENT_A_FILE,
+            FileMode.Open,
+            FileAccess.Read,
+            FileShare.ReadWrite);
+
+         var irq = new byte[4];
+        var count = fs.Read(irq, 0, 4); // blockiert, bis IRQ kommt
+        Console.WriteLine($"IRQ count: {BitConverter.ToUInt32(irq)}");
+
         // using (var mmf = MemoryMappedFile.CreateFromFile(PcieDevice.CSR_RESOURCE_FILE, FileMode.Open, null,8 ))
         // {
         //     using (var accessor = mmf.CreateViewAccessor(PcieDevice.SR_OFFSET, 4))
--- LiteFury.Acquisition/LiteFury.Acquisition.Core/Hardware/PcieDevice.cs
+++ LiteFury.Acquisition/LiteFury.Acquisition.Core/Hardware/PcieDevice.cs
@@ -7,7 +7,10 @@ public static class PcieDevice
 //   Sysfs path to the PCIe BAR2 resource, memory-mapped for register/BRAM access.
     public const string PCI_RESOURCE2_PATH = "/sys/bus/pci/devices/0000:01:00.0/resource2";
 
-// use only with mmap xdma0_bypass has no engine! pread, pwrite, ... needs an engine to run otherwise it returns (EINVAL)
+// use only with mmap, xdma0_bypass has no DMA engine 
+// do not use pread, pwrite, ... with xdma0_bypass, otherwise it returns EINVAL
+// read returns the DMA descriptors of the engine, not CSR/BAR data
+// write writes descriptor data to the DMA bypass BAR
     public const string CSR_RESOURCE_FILE = "/dev/xdma0_bypass";
     public const string USR_IRQ_EVENT_A_FILE = "/dev/xdma0_events_0";
     public const string USR_IRQ_EVENT_B_FILE = "/dev/xdma0_events_1";
@@ -17,9 +20,11 @@ public static class PcieDevice
     public const string BRAM_RESOURCE_FILE_DMA = "/dev/xdma0_c2h_0";
     public const long SR_BASE_DMA = 0x44A00000;
     public const long BRAM_BASE_DMA = 0x44A02000;
-    
+
     public const long BRAM_A_BASE_DMA = BRAM_BASE_DMA;
-    public const long BRAM_B_BASE_DMA = BRAM_BASE_DMA + (BRAM_WORDS/2 * sizeof(uint));
+
+    public const long BRAM_B_BASE_DMA = BRAM_BASE_DMA + (BRAM_WORDS / 2 * sizeof(uint));
+
     // /////////////////////////
     public const int ENABLE_ACQ_BIT = 0;
     public const int SOFT_RESET_BIT = 1;
@@ -32,7 +37,4 @@ public static class PcieDevice
     public const int SR_OFFSET = 0x04;
     public const int CR_OFFSET = 0x00;
     public const int BRAM_OFFSET = 0x2000;
-
-
-
 }
