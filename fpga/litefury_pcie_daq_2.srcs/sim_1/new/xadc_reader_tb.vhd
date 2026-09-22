c4769eb0d1366d791d4fbfb72d165cea2646ff5c
Al_Niz <32996909+sanfour2011@users.noreply.github.com>
2026-09-22 12:36:51 +0200

    wip: add missed xadcconfig  testbench

--- fpga/litefury_pcie_daq_2.srcs/sim_1/new/xadc_reader_tb.vhd
+++ fpga/litefury_pcie_daq_2.srcs/sim_1/new/xadc_reader_tb.vhd
@@ -35,6 +35,13 @@ entity xadc_reader_tb is
 end xadc_reader_tb;
 
 architecture Behavioral of xadc_reader_tb is
+
+	constant AVG_VALUE     : std_logic_vector(1 downto 0) := "10";
+	constant CFG0_INIT     : std_logic_vector(15 downto 0) := x"DEAD";
+	constant CFG0_EXPECTED : std_logic_vector(15 downto 0) := CFG0_INIT(15 downto 14) & AVG_VALUE & CFG0_INIT(11 downto 0);
+	-- DRP addresses
+	constant ADDR_CFG0 : std_logic_vector(6 downto 0) := "1000000";  -- 0x40 config register 0 
+
 	signal clk_sig             : std_logic := '0';
 	signal rst_n_sig           : std_logic;
 	signal drdy_sig            : std_logic := '0';
@@ -46,7 +53,19 @@ architecture Behavioral of xadc_reader_tb is
 	signal dwe_sig             : std_logic := '0';
 	signal temperature_out_sig : std_logic_vector(31 downto 0) := (others => '0');
 	signal reset_test_done     : boolean := false;
+	signal config_test_done    : boolean := false;
+	signal read_test_done      : boolean := false;
+	signal drdy_cfg, drdy_rd   : std_logic := '0';
+	signal do_cfg, do_rd       : std_logic_vector (15 downto 0) := (others => '0');
+
+	type test_t is (TEST_RESET, TEST_CONFIG_XADC, TEST_READ_SAMPLE, TEST_FINISHED);
+	signal current_test : test_t := TEST_RESET;
+
 begin
+	
+	drdy_sig <= drdy_cfg when current_test = TEST_CONFIG_XADC else drdy_rd;
+	do_sig <= do_cfg     when current_test = TEST_CONFIG_XADC else do_rd;
+
 	UUT : entity work.xadc_reader
 	port map (
 		clk              => clk_sig,
@@ -89,11 +108,54 @@ begin
 			wait;
 		end process reset_process;
 
-		FSM_seq : process
-		variable output_15 : std_logic_vector(15 downto 0);
-
+		sequencer_process : process
 		begin
-			wait until reset_test_done = true;
+			wait until reset_test_done;
+			current_test <= TEST_CONFIG_XADC;
+			wait until config_test_done;
+			current_test <= TEST_READ_SAMPLE;
+			wait until read_test_done;
+			current_test <= TEST_FINISHED;
+			report "xadc_reader_tb: all tests finished" severity note;
+			wait;
+		end process sequencer_process;
+
+		config_xadc_test_process : process
+		begin
+			wait until current_test = TEST_CONFIG_XADC;
+
+			-- 1st transfer: read of config register 0
+			wait until rising_edge(clk_sig) and den_sig = '1';
+			assert daddr_sig = ADDR_CFG0 report "Config Error: first transfer must address 0x40!" severity error;
+			assert dwe_sig = '0' report "Config Error: first transfer must be a read (dwe = '0')!" severity error;
+			do_sig <= CFG0_INIT; -- answer the read
+			drdy_sig <= '1';
+			wait until rising_edge(clk_sig);
+			drdy_sig <= '0';
+
+			-- 2nd transfer: write back with the new averaging bits
+			wait until rising_edge(clk_sig) and den_sig = '1';
+			assert daddr_sig = ADDR_CFG0 report "Config Error: write must address 0x40!" severity error;
+			assert dwe_sig = '1' report "Config Error: second transfer must be a write (dwe = '1')!" severity error;
+			assert di_sig(13 downto 12) = AVG_VALUE report "Config Error: bits 13:12 must be avg!" severity error;
+			assert di_sig = CFG0_EXPECTED report "Config Error: all other bits must stay as read!" severity error;
+			drdy_sig <= '1'; -- acknowledge the write
+			wait until rising_edge(clk_sig);
+			drdy_sig <= '0';
+
+			-- FSM back to S_IDLE, hand drdy and do over to the next test
+			wait until rising_edge(clk_sig);
+			wait until rising_edge(clk_sig);
+			drdy_sig <= 'Z';
+			do_sig <= (others => 'Z');
+			config_test_done <= true;
+			wait;
+		end process config_xadc_test_process;
+
+		read_xadc_test_process : process
+		variable output_15 : std_logic_vector(15 downto 0);
+		begin
+			wait until current_test = TEST_READ_SAMPLE;
 			wait until rising_edge(clk_sig); --S_IDLE
 			eoc_sig <= '1';
 			wait until rising_edge(clk_sig); --S_PULSE_DEN
@@ -120,7 +182,8 @@ begin
 			wait for 1 ns;
 			assert temperature_out_sig = std_logic_vector'(31 downto 12 => '0') & output_15(15 downto 4)
 			report "FSM Error: temperature_out must be 0!" severity error;
+			read_test_done <= true;
 			wait;
-		end process FSM_seq;
+		end process read_xadc_test_process;
 
 	end Behavioral;
